e9cfbca5658e53c2427c1792ffabb89e484096de
Edt94 <greenape2002@gmail.com>
2023-10-06 14:57:39 +0300

    prod database change

--- database.py
+++ database.py
@@ -2,11 +2,16 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./todos.db"
+SQLALCHEMY_DATABASE_URL = "postgresql://wudkfjug:M71bebQQl4MS3EQGr1TW-2NZ3Dc_QJJL@cornelius.db.elephantsql.com/wudkfjug"
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+
+# SQLALCHEMY_DATABASE_URL = "sqlite:///./todos.db"
+#
+# engine = create_engine(
+#     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+# )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
